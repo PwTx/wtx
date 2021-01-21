dfd07abf0bf4ee7dfe472ec8970854607a1e08dc
Tiago Alves <tiagocbf0000@gmail.com>
2021-01-21 19:58:25 -0300

    First commit

---
+++ src/pack.js
@@ -0,0 +1,25 @@
+const pack = (prefix) => {
+    return `PASTA DO MEGA COM DIVERSOS VIDEOS E FOTOS PORN:
+
+🔞 FOTOS DE HENTAI 720MB: https://encurta.eu/YU7k 🔞
+
+🔞 PRINCESS STARLING(ONLY FANS)8.36GB: https://encurta.eu/cBiTfDBd 🔞
+
+🔞 ELLES CLUB 28.68GB: https://encurta.eu/ESTyP 🔞
+
+🔞 COCONUTKITTY(ONLY FANS)7.69GB: https://encurta.eu/U2I5🔞
+
+🔞 Belle delphine : https://photos.app.goo.gl/Jr4Qk1dFSJepPdRc7 🔞
+
+🔞 Pack1: https://photos.app.goo.gl/phyRpNFLcAtsknfJ7 🔞
+
+🔞 Kitty kum: https://photos.app.goo.gl/gNxLbJHGVzeWY9iS9 🔞
+
+🔞 Misaki Your Waifu: https://photos.app.goo.gl/M9nyHdonAJu5GRjZ9 🔞
+
+🔞 pack: https://photos.app.goo.gl/SrogbCbnanL2PJQn7 🔞
+
+🔞 Traveco Da Foto : https://wa.me/+5519996533979 🔞
+`
+}
+exports.pack = pack
